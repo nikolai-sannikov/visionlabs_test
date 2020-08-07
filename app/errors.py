a815d03261925779dc5f319594c9d2084f8d2113
Nikolai Sannikov <kolyambosunny@gmail.com>
2020-08-07 11:17:31 -0400

    Improved code, made exceptions more uniform

--- app/errors.py
+++ app/errors.py
@@ -1,46 +1,51 @@
-from flask import Blueprint, jsonify
-from werkzeug.exceptions import HTTPException
 import logging
+
+from flask import Blueprint, jsonify
+from werkzeug.exceptions import HTTPException, InternalServerError
+
 
 log = logging.getLogger("ImagesLogger")
 errors = Blueprint('errors', __name__)
 
-class InvalidUsage(Exception):
-    status_code = 400
 
-    def __init__(self, message, status_code=None, payload=None):
+class InvalidUsage(Exception):
+    def __init__(self, description, status_code=400, payload=None):
         Exception.__init__(self)
-        self.message = message
-        if status_code is not None:
-            self.status_code = status_code
+        self.description = description
+        self.status_code = status_code or 400
         self.payload = payload
 
     def to_dict(self):
-        rv = dict(self.payload or ())
-        rv['message'] = self.message
-        return rv
+        result = {'args': self.payload}
+        result['message'] = self.description
+        return result
 
     @property
     def args(self):
         return self.payload
 
+
 def get_error_summary(error):
     error_summary = {
             'type': error.__class__.__name__,
-            'message': getattr(error, 'message', ''),
+            'message': getattr(error, 'description', ''),
             'args': [str(x) for x in error.args]
         }
     return error_summary
 
 
 @errors.app_errorhandler(HTTPException)
-def handle_werkzeug_exception(e):    
+def handle_werkzeug_exception(e):
     # replace the body with JSON
-    response = {        
-        "type": e.name,
-        "message": e.description,        
+    response = {
+        "success": False,
+        "error": {
+            "type": e.name,
+            "message": e.description
+        }
     }
-    return jsonify(response) ,e.code
+    return jsonify(response), e.code
+
 
 @errors.app_errorhandler(InvalidUsage)
 def handle_expected_error(error):
@@ -52,17 +57,10 @@ def handle_expected_error(error):
     }
     return jsonify(response), status_code
 
+
 @errors.app_errorhandler(Exception)
-def handle_unexpected_error(error):
-    
-    log.exception(get_error_summary(error))
-    #provide real status_code to the public
-    status_code = getattr(error, 'status_code', 500)
-    public_response = {
-        'success': False,
-        'error': {
-            'type': "UnknownServerException",
-            'message': "Something went wrong"
-        }
-    }
-    return jsonify(public_response), status_code
+def handle_unexpected_exception(exception):
+    # write a real exception to logs
+    log.exception(get_error_summary(exception))
+    # give a non-informative error to the public
+    raise InternalServerError("Something went wrong")
--- app/image_utils.py
+++ app/image_utils.py
@@ -1,17 +1,29 @@
 from io import BytesIO
 from PIL import Image
-import base64
+from base64 import b64encode, b64decode
 
-def encode_image_base64(pillow_image):        
+
+def encode_image_base64(pillow_image):
+    """Encodes image in a base64 string with UTF-8 representation
+    Uses Pillow built-in function to convert image to a JPEG byte array.
+    """
     byteIOBuffer = BytesIO()
+    # write image in JPEG format to a byte buffer
     pillow_image.save(byteIOBuffer, format='JPEG')
+    # flush IO buffer to an array
     byte_array = byteIOBuffer.getvalue()
 
-    base64_encoded = base64.b64encode(byte_array)
+    base64_encoded = b64encode(byte_array)
+    # use string utf-8 representation for usage convenience
     return str(base64_encoded.decode("utf-8"))
 
-def decode_image_base64(base64_string):  
-    base64_decoded = base64.b64decode(base64_string)
 
-    decoded_image = Image.open(BytesIO(base64_decoded))
+def decode_image_base64(base64_string):
+    """Decodes image from a base64 encoded string.
+    Uses built-in Pillow function to get an image from decoded bytes.
+    If the data is corrupted (or not an image), Pillow will fail to do that.
+    """
+    bytes_decoded = b64decode(base64_string)
+    # load image from IO buffer filled from base64 decoding
+    decoded_image = Image.open(BytesIO(bytes_decoded))
     return decoded_image
